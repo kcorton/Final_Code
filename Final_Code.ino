a340035e5e4de0eadd50306c3bbc82aa126d8c57
Karen Orton <kcorton@wpi.edu>
2015-03-30 15:34:54 -0400

    Initial Structure

--- Final_Code.ino
+++ Final_Code.ino
@@ -1,3 +1,49 @@
 // Final Code Goes Here 
 
-//This is a test
+/*********************************************************************************************/
+/* Necessary Include Files */
+
+
+#define findFire 0
+#define extinguishFire 1
+#define returnHome 2
+
+
+void setup(){
+  
+  
+}
+
+void loop() {
+  switch (mainState) {
+    case findFire:
+      findFire();
+    case extinguishFire:
+      extinguishFire();
+    case returnHome:
+      returnHome();
+  }
+}
+
+
+/*********************************************************************************************/
+// FindFire Switch State
+
+void findFire(void) {
+  
+}
+
+/*********************************************************************************************/
+//Extinguish Fire Switch Statement
+
+void extinguishFire (void){
+
+
+}
+
+/*********************************************************************************************/
+// Return Home Switch Statement
+
+void returnHome (void) {
+  
+}
