28869edc21d1bf6281dcd191b1bd563adeb3cb85
bshappell <bshappell@wpi.edu>
2015-04-06 13:10:08 -0400

    Fire Sensor Testing

---
+++ fireSensorTesting/fireSensorTesting.ino
@@ -0,0 +1,37 @@
+#include <Servo.h> 
+ 
+Servo myservo;
+
+#define firePin A0
+
+int pos = 0;    // variable to store the servo position 
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val;
+  float dist;
+  
+  
+    for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+    
+    val = analogRead(firePin);
+  Serial.println(val);
+  } 
+  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+    
+    val = analogRead(firePin);
+  Serial.println(val);
+  } 
+
+}
