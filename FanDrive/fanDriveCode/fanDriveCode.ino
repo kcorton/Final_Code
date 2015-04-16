1834399680188f5e1b7347e804548bdee83f06e8
csinkler <charles.s.745@gmail.com>
2015-04-16 14:44:04 -0400

    Changed to support state machine
    Needs to have a lot of the setup things moved to the switch to this
    state statement. Thats confusing I know, but karen you know what i mean.

--- FanDrive/fanDriveCode/fanDriveCode.ino
+++ FanDrive/fanDriveCode/fanDriveCode.ino
@@ -1,3 +1,5 @@
+#include <TimerOne.h>
+
 /*Charlie wrote this code. It runs the fan, and at the same time, raises the fan arm, then lowers it.
 once the fan is lowered, the fan should continue spinning. To stop this, add analogWrite(fanMotor, 0); in the 
 change state, along with the boolean hitTop = false; This will essentially reset all the variables for this state,
@@ -5,37 +7,59 @@ which would be good for using a state machine.*/
 
 #include<Servo.h>
 
+//===========================Pin Attachments===============================================
+
 int fanMotor = 22;
 Servo armMotor;
 int armPot = A4;
-int highPos = 158;
-int lowPos = 115;
-boolean hitTop = false; //need to use this line within the change state, to reset to false.
+
+//=============================Variables===================================================
+
+int armState = 0;     // most of these need to be put in the switch to this state statement
+int armTimePassed, armInitTime;
+int armWaitTime = 100;
+int highPos = 460;
+int lowPos = 320;
+int countTime = 0;
+boolean hitTop = false;
 
 //==========================================================================================
 void setup(){
+  Timer1.initialize(100000); // interrupt every .1s or 10 times every second
+  Timer1.attachInterrupt(timerISR);
   armMotor.attach(4);
   pinMode(armPot, INPUT);
+  armInitTime = countTime;
   Serial.begin(9600);
+  
 }
 
 //==========================================================================================
 
 void loop(){
-  analogWrite(fanMotor, 255);
-  if(!hitTop){
-    armMotor.write(70);
+  Serial.println(analogRead(armPot));
+  armTimePassed = countTime - armInitTime;
+  switch(armState){
+    case 0:
     if(analogRead(armPot) > highPos){
-      hitTop = true;
+      armMotor.write(100);
     }
+    else{
+      armMotor.write(108);
+    }
+    if(armTimePassed > armWaitTime){
+      armState = 1;
+    }
+    break;
+    
+    case 1:
+    armMotor.write(93);
+    break;
   }
-  else if(analogRead(armPot) > lowPos){
-    armMotor.write(110);
-  }
-  else{
-    armMotor.write(90);
-  }
-  Serial.println(hitTop);
+}
+
+void timerISR(){
+  countTime++;
 }
 
   
