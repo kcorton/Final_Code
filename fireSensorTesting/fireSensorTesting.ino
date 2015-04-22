02146cdca74a9c4642cf2d572f8906c2d4c37d94
bshappell <bshappell@wpi.edu>
2015-04-21 20:00:11 -0400

    final fire servo code
    two needed fire finding functions

--- fireSensorTesting/fireSensorTesting.ino
+++ fireSensorTesting/fireSensorTesting.ino
@@ -1,45 +1,52 @@
 #include <Servo.h> 
+#include <TimerOne.h>
  
-Servo myservo;
+Servo flameServo;
 
-#define firePin A0
+#define firePin A1
+#define extinguishingFire 1
+int mainState = 0;
+int servoIncreasing = 0;
+int fireServoPos = 0;
+volatile long countTime = 0;
+int scanComplete = false;
+long lastFireTimeCount = 0;
 
-int pos = 0;    // variable to store the servo position 
+int firePosition; 
+int lastFlameVal = 2000;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  flameServo.attach(9, 544, 2400);
+  
+  Timer1.initialize(10000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
+  Timer1.attachInterrupt( timerIsr ); // attach the service routine here
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int val;
-  float dist;
-  
-  
-    for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-    
-    val = analogRead(firePin);
-  Serial.println(val);
-  } 
-  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-    
-    val = analogRead(firePin);
-  Serial.println(val);
-  } 
 
+  switch(mainState) {
+  
+  case 0: 
+    lookForFire();
+    break;
+  case 1:
+    scan();
+    if(scanComplete) {
+      scanComplete = false;
+      delay(5000);
+    }
+    break;
+    
+  }
 }
 
-// Can you put any setup and intialization info that is needed for this to work Below here 
-
-#define extinguishingFire 1
-int mainState = 0;
+void timerIsr(void) {
+  
+  countTime++;
+  
+}
 
 
 /*********************************************************************************************/
@@ -48,21 +55,105 @@ int mainState = 0;
 // and saving a global variable with the servo position at the highest flame sensing
 // this is used once the flame has been seen
 
-void scan (void) {
-
-
+void scan(void) {
+  
+  int currFlameVal;
+  
+  if(countTime - lastFireTimeCount > 28) {
+  
+    if(fireServoPos > 180) {
+      
+      fireServoPos = 0; // reset the position
+      lastFlameVal = 2000; // reset the last flame val
+      flameServo.write(0); // write the servo back to reset position
+      scanComplete = true; //Indicate the scan is complete
+      
+      Serial.println("DONEEEEE");
+      Serial.println(firePosition);
+      
+    }
+    
+    else {
+      
+      flameServo.write(fireServoPos);
+      currFlameVal = analogRead(firePin);
+      
+      Serial.print("Position ");
+      Serial.print(fireServoPos);
+      Serial.print("Flame Value ");
+      Serial.println(currFlameVal);
+      
+      if(currFlameVal < lastFlameVal) {
+        
+        lastFlameVal = currFlameVal;
+        firePosition =  fireServoPos;
+      
+      }
+      
+      fireServoPos += 10;
+      
+    }
+      
+      lastFireTimeCount = countTime; // reset the stored time variable
+    
+  }
+    
 }
 
 /*********************************************************************************************/
 // Look For Fire function
 
+
 // This function is constantly polled when navigating the maze
 // it rotates the servo and if it ever sees a "flame" changes the main State 
 
-void lookForFire (void) {
+void lookForFire(void) {
   
-  // if the fire has been found 
-   mainState = extinguishingFire;
+  int flameVal;
+  
+  if(countTime - lastFireTimeCount >= 15) {
+    
+    /* Read current fire sensor value */
+    flameVal = analogRead(firePin);
+    Serial.println(flameVal);
+    //Serial.println(fireServoPos);
+  
+    flameServo.write(fireServoPos);
+    
+    if(servoIncreasing) {
+      if(fireServoPos <= 170) {
+        fireServoPos += 10; 
+        flameServo.write(fireServoPos);     
+      }
+      else {
+        servoIncreasing = 0;
+      }
+    }
+    else {
+      if(fireServoPos >= 10) {
+        fireServoPos -= 10;
+        flameServo.write(fireServoPos);
+      }
+      else {
+        servoIncreasing = 1;
+      }
+    }
+    
+  
+    /* If the flame is found */
+    if(flameVal < 200) {
+       /* Change to extiguish fire state */
+       mainState = extinguishingFire;
+       fireServoPos = 0; // reset the position
+       lastFlameVal = 2000; // reset the last flame val
+       flameServo.write(0); // write the servo back to reset position
+       scanComplete = true; //Indicate the scan is complete
+       Serial.println("Fire Found");
+    }
+
+    lastFireTimeCount = countTime; // reset the stored time variable
+
+  }
   
 }
 
