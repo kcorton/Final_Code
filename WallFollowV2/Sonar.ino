f14688d53624eab5c03023e1e40ad47cf0ef0535
zenofchaos <zenofchaos@comcast.net>
2015-04-23 16:04:51 -0400

    Back Sonar Polling
    Back sonar changed to polling because there are not enough interrupt
    pins to support it. Also included code to ping different sensors
    depending on the overall state of the program (findingFire activates the
    front and side sonar, while returningHome activates the side and back).

---
+++ WallFollowV2/Sonar.ino
@@ -0,0 +1,206 @@
+//Contains all functions pertaining to running/using the sonar
+//sonar pins
+int frontPingPin = 27;
+int sidePingPin = 28;
+int backPingPin = 29;
+int frontEchoPin = 18;
+int sideEchoPin = 19;
+int backEchoPin = 26;
+
+long frontPingTime = 0;
+long sidePingTime = 0;
+long backPingTime = 0;
+
+volatile long frontEchoTime = 0;
+volatile long sideEchoTime = 0;
+volatile long backEchoTime = 0;
+
+int tempEchoFront = 0;
+int tempEchoSide = 0;
+int tempEchoBack = 0;
+
+boolean waiting = false;
+
+int pingNext = FRONTSONAR;
+
+#define findingFire 0
+#define exstinguishingFire 1
+#define returningHome 2
+#define arrivedHome 3
+int mainState = 2;
+//========================================================
+void initializeSonar(){
+  //Sonar pin declarations
+  pinMode(frontPingPin,OUTPUT);
+  pinMode(frontEchoPin,INPUT);
+  pinMode(sidePingPin,OUTPUT);
+  pinMode(sideEchoPin,INPUT);
+  pinMode(backPingPin,OUTPUT);
+  pinMode(backEchoPin,INPUT);
+  
+  //Sonar interrupt initialization
+  attachInterrupt(5,frontSonarISR,CHANGE); 
+  attachInterrupt(4,sideSonarISR,CHANGE);  
+//  attachInterrupt(3,backSonarISR,CHANGE);  
+  
+  //Ensures all Soonar pin pins are initialized to low
+  digitalWrite(frontPingPin,LOW);
+  digitalWrite(sidePingPin,LOW);
+  digitalWrite(backPingPin,LOW);
+}
+int getNextPing(){
+  return pingNext; 
+}
+//=======================================================
+float getDis(int sonarToRead){
+  switch(sonarToRead){
+    case FRONTSONAR:{
+       return ( (567.5 * ( (float)frontEchoTime/1000000)) * 12.0);
+    } 
+    case SIDESONAR:{
+       return ( (567.5 * ( (float)sideEchoTime/1000000)) * 12.0);
+    } 
+    case BACKSONAR:{
+       return ( (567.5 * ( (float)backEchoTime/1000000)) * 12.0);
+    } 
+  }
+}
+//========================================================
+void ping(int sensorToPing){
+  if(!waiting){
+    int pingPin;
+    switch(sensorToPing){
+      case FRONTSONAR:{
+        pingPin = frontPingPin;
+        break;
+      }
+      case SIDESONAR:{
+        pingPin = sidePingPin;
+        break;
+      }
+      case BACKSONAR:{
+        pingPin = backPingPin;
+        break;
+      }
+    }
+      // Serial.print(pingPin);
+  
+    delay(50);  //ensures a second ping is not sent out before 
+                //the first returns
+    digitalWrite(pingPin,LOW);
+    digitalWrite(pingPin,HIGH);
+    delayMicroseconds(250);
+    digitalWrite(pingPin,LOW); 
+   
+    if(sensorToPing == BACKSONAR){
+      backEchoTime = pulseIn(backEchoPin,HIGH);
+      switch(mainState){
+        case findingFire:{
+          pingNext = FRONTSONAR;
+          break;
+        }
+        case exstinguishingFire:{
+          pingNext = FRONTSONAR;
+          break;
+        }
+        case returningHome:{
+          pingNext = SIDESONAR;
+          break;
+        }
+        case arrivedHome:{
+          pingNext = FRONTSONAR;
+          break;
+        }
+      }
+    }
+    else{
+      waiting = true;
+    }
+    
+  }
+}
+//========================================================
+void frontSonarISR(){
+  if(digitalRead(frontEchoPin) == HIGH){
+    frontPingTime = micros();
+  }
+  else{
+    tempEchoFront = micros() - frontPingTime;
+    if(tempEchoFront < 30000){
+      frontEchoTime = tempEchoFront;
+      switch(mainState){
+        case findingFire:{
+          pingNext = SIDESONAR;
+          break;
+        }
+        case exstinguishingFire:{
+          pingNext = FRONTSONAR;
+          break;
+        }
+        case returningHome:{
+          pingNext = SIDESONAR;
+          break;
+        }
+        case arrivedHome:{
+          pingNext = FRONTSONAR;
+          break;
+        }
+      }
+    }
+    else{
+      pingNext = FRONTSONAR; 
+    }
+    waiting = false; 
+  }
+}
+//========================================================
+void sideSonarISR(){
+  if(digitalRead(sideEchoPin) == HIGH){
+    sidePingTime = micros();
+  }
+  else{
+    tempEchoSide = micros() - sidePingTime;
+    if(tempEchoSide < 30000){
+      sideEchoTime = tempEchoSide;
+      switch(mainState){
+        case findingFire:{
+          pingNext = FRONTSONAR;
+          break;
+        }
+        case exstinguishingFire:{
+          pingNext = FRONTSONAR;
+          break;
+        }
+        case returningHome:{
+          pingNext = BACKSONAR;
+          break;
+        }
+        case arrivedHome:{
+          pingNext = FRONTSONAR;
+          break;
+        }
+      }
+    }
+    else{
+      pingNext = SIDESONAR; 
+    }
+    waiting = false;
+  }
+}
+//========================================================
+//void backSonarISR(){
+//  if(digitalRead(backEchoPin) == HIGH){
+//    backPingTime = micros();
+//  }
+//  else{
+//    tempEchoBack = micros() - backPingTime;
+//    if(tempEchoBack < 30000){
+//      backEchoTime = tempEchoBack;
+//      pingNext = FRONTSONAR;
+//    }
+//    else{
+//      pingNext = FRONTSONAR; 
+//    }
+//    waiting = false;
+//  }
+//}
---
+++ WallFollowV2/WallFollowV2.ino
@@ -0,0 +1,40 @@
+#include <TimerOne.h>
+
+#define FRONTSONAR 1
+#define SIDESONAR 2
+#define BACKSONAR 3
+
+int fanPin = 22;
+
+long countTime = 0;
+
+//==========================================================================
+void setup(){
+  Serial.begin(9600);
+  
+  //timer set up
+  Timer1.initialize(100000);	//0.1 seconds               
+  Timer1.attachInterrupt(timerISR);
+  
+  pinMode(fanPin, OUTPUT);
+  digitalWrite(fanPin,LOW);
+  
+  initializeMotors();
+  
+  initializeSonar();
+}
+//==========================================================================
+void loop(){
+  //followWall();
+  ping(getNextPing());
+  Serial.print(getDis(FRONTSONAR));
+  Serial.print("  ");
+  Serial.print(getDis(SIDESONAR));
+  Serial.print("  ");
+  Serial.println(getDis(BACKSONAR));
+}
+//==========================================================================
+void timerISR(void) {
+  incrementAccelTime();
+  countTime++;
+}
